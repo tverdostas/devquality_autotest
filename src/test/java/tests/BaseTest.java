ee41105582744a5a9ee43fd7d2a65a98f0d4adb4
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-09-06 21:37:31 +0400

    homework in progress - allure report delivered to telegram

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTest {
+    @BeforeAll
+    static void browserConfigurations(){
+        Configuration.baseUrl = "https://tatarstan.mts.ru/personal";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-setuid-sandbox");
+
+        options.addArguments("--disable-application-cache");
+        options.addArguments("--enable-do-not-track");
+        options.addArguments("--disable-popup-blocking");
+
+        Configuration.browserCapabilities = options;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
+    }
+
+    @AfterEach
+    public void closeWebDriver(){
+        Selenide.closeWebDriver();
+    }
+}
--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,92 +1,57 @@
 package tests;
 
-import com.codeborne.selenide.*;
 import enums.MiddleMenu;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import pages.MainPage;
 
-public class MainPageTests {
-    @BeforeAll
-    static void browserConfigurations(){
-        Configuration.baseUrl = "https://tatarstan.mts.ru/personal";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
-    }
-
-    @AfterEach
-    public void closeWebDriver(){
-        Selenide.closeWebDriver();
-    }
+public class MainPageTests extends BaseTest {
 
     MainPage mainPage = new MainPage();
     @Test
     void startPageIsPersonalClientsTest() {
-        mainPage.
-                openPage().
-                getMainHeaderButton().
-                checkMainHeaderButton();
+        mainPage
+                .openPage()
+                .getMainHeaderButton()
+                .checkMainHeaderButton();
     }
-
-/*    @ParameterizedTest
-    @EnumSource(MiddleMenu.class)
-    void allButtonsInMiddleMenuIsVisibleTest(MiddleMenu middleMenu) {
-        open("https://tatarstan.mts.ru/personal");
-
-        ElementsCollection elements = $$x("//div[@class='navigation-menu-slider__container']/descendant::h3[text()='" + middleMenu.getDisplayName() + "' and @class='navigation-menu-card__title']");
-        elements.filter(visible);
-    }*/
 
     @ParameterizedTest
     @EnumSource(MiddleMenu.class)
     void allButtonsInMiddleMenuIsVisibleWithAssertionTest(MiddleMenu middleMenu) {
-        mainPage.
-                openPage().
-                verifyMenuItemIsVisible(middleMenu.getDisplayName());
+        mainPage
+                .openPage()
+                .verifyMenuItemIsVisible(middleMenu.getDisplayName());
     }
 
     @Test
     void modalWindowPersonalAccountHaveCorrectHeaderTest(){
-        mainPage.
-                openPage().
-        clickSignInButton().
-        mainHeaderIsVisible();
+        mainPage
+                .openPage()
+                .clickSignInButton()
+                .mainHeaderIsVisible();
     }
-/*    @Test
-    void modalWindowReplenishmentOfBalanceHaveCorrectHeaderTest(){
-        open("https://tatarstan.mts.ru/personal");
-        $x("//span[contains(@class,'mm-web-button__label') and text()='Пополнить']").click();
-        $x("//h2[text()='Личные кабинеты']");
-  }*/
 
     @Test
     void fieldWithPhoneNumberInBalanceHaveCorrectPlaceholderTest(){
-        mainPage.
-                openPage().
-                phoneNumberInReplenishmentOfBalanceHaveCorrectPlaceholder();
+        mainPage
+                .openPage()
+                .phoneNumberInReplenishmentOfBalanceHaveCorrectPlaceholder();
     }
 
     @Test
     void fieldWithPhoneNumberMoveToMtsHaveCorrectPlaceholderTest(){
-        mainPage.
-                openPage().
-                phoneNumberInMoveToMtsBlockHaveCorrectPlaceholder();
+        mainPage
+                .openPage()
+                .phoneNumberInMoveToMtsBlockHaveCorrectPlaceholder();
     }
 
     @Test
     void submitButtonIsDisabledForEmptyMoveToMtsTest(){
-/*        SelenideElement submitButton = $x("//span[text()='Перейти в МТС']/ancestor::button[@type='submit']");
-
-        open("https://tatarstan.mts.ru/personal");
-        submitButton.click();
-        submitButton.shouldHave(attribute("disabled", ""));*/
-
-        mainPage.
-                openPage().
-                submitButtonMoveToMtsClick().
-                submitButtonMoveToMtsCheckIsDisabled();
+        mainPage
+                .openPage()
+                .submitButtonMoveToMtsClick()
+                .submitButtonMoveToMtsCheckIsDisabled();
     }
 }
